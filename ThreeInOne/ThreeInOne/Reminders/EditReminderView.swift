a4b43d8e9f8d81e7ba821dde9e265814de2ee32f
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-11 19:29:19 +0530

    Repositioned the dismiss button from the toolbar to the bottom in an HStack with the save changes button.

--- ThreeInOne/ThreeInOne/Reminders/EditReminderView.swift
+++ ThreeInOne/ThreeInOne/Reminders/EditReminderView.swift
@@ -56,46 +56,47 @@ struct EditReminderView: View {
                 }
             }
             .navigationTitle("Edit Todo")
-            .toolbar {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "xmark")
-                        .padding(10)
-                        .font(.caption)
-                        .fontWeight(.bold)
-                        .foregroundStyle(Color.white)
-                        .background(Color.secondary)
-                        .clipShape(Circle())
-                        .padding(.top)
-                }
-            }
         }
         .interactiveDismissDisabled()
 
-        Button {
-            if name.trimmingCharacters(in: .whitespaces) == "" {
-                name = "New Todo"
+        HStack {
+            // To save changes button
+            Button {
+                if name.trimmingCharacters(in: .whitespaces) == "" {
+                    name = "New Todo"
+                }
+                
+                DataController().editReminder(reminder: reminder, name: name, reminder_desc: reminder_desc, tags: tags, context: managedObjectContext)
+                dismiss()
+            } label: {
+                Label("Save Changes", systemImage: "plus")
+            }.disabled(disabledEditButton)
+            .padding()
+            .font(.title3)
+            .fontWeight(.bold)
+            .foregroundStyle(Color.newFont)
+            .background(!disabledEditButton ? Color(UIColor(hex: customTabViewModel.tabBarItems[1].accentColor)) : Color(UIColor(hex: "DEDEE0")))
+            .clipShape(RoundedRectangle(cornerRadius: 1000.0))
+            .onAppear {
+                isCustomTabBarHidden = true
+                isAddButtonHidden = true
+            }
+            .onDisappear {
+                isCustomTabBarHidden = false
+                isAddButtonHidden = false
             }
             
-            DataController().editReminder(reminder: reminder, name: name, reminder_desc: reminder_desc, tags: tags, context: managedObjectContext)
-            dismiss()
-        } label: {
-            Label("Add Changes", systemImage: "plus")
-        }.disabled(disabledEditButton)
-        .padding()
-        .bold()
-        .font(.title3)
-        .background(!disabledEditButton ? Color(UIColor(hex: customTabViewModel.tabBarItems[1].accentColor)) : Color(UIColor(hex: "DEDEE0")))
-        .foregroundStyle(Color.newFont)
-        .clipShape(RoundedRectangle(cornerRadius: 1000.0))
-        .onAppear {
-            isCustomTabBarHidden = true
-            isAddButtonHidden = true
-        }
-        .onDisappear {
-            isCustomTabBarHidden = false
-            isAddButtonHidden = false
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "xmark")
+                    .padding()
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .background(.tertiary)
+                    .foregroundStyle(Color.white)
+                    .clipShape(Circle())
+            }
         }
     }
     
