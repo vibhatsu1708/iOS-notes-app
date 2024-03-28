5883c0e29a65bfc848939de53f58d720a29e630b
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-29 03:38:52 +0530

    Implemented the swipe to unlock functionality in SwiftUI, for the application, so now the user can just swipe to unlock to open the application. Later will add an option to disable that.

--- ThreeInOne/ThreeInOne/MainViews/SplashScreenView.swift
+++ ThreeInOne/ThreeInOne/MainViews/SplashScreenView.swift
@@ -8,40 +8,44 @@
 import SwiftUI
 
 struct SplashScreenView: View {
-    @State private var isActive: Bool = false
+    @State private var showUnlockedView: Bool = false
+    @State private var didUnlock: Bool = false
     
     var body: some View {
-        if isActive {
-            ContentView()
-        } else {
+        
+        ZStack {
+            Color.black
+            
             VStack {
-                VStack(alignment: .leading, spacing: 1) {
-                    Text("iSimplify")
-                        .font(.system(size: 40))
-                        .foregroundStyle(Color(UIColor(hex: "232528")))
-                        .fontWeight(.heavy)
-                    HStack(spacing: 5) {
-                        Group {
-                            Circle()
-                                .foregroundStyle(Color(UIColor(hex: "6AB547")))
-                            Circle()
-                                .foregroundStyle(Color(UIColor(hex: "F64740")))
-                            Circle()
-                                .foregroundStyle(Color(UIColor(hex: "8A4FFF")))
+                Spacer()
+                
+                Text("Simplify")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color.white)
+                
+                Spacer()
+                if showUnlockedView {
+                    SwipeToUnlockView()
+                        .onSwipeSuccess {
+                            withAnimation(Animation.easeInOut) {
+                                self.didUnlock = true
+                                self.showUnlockedView = false
+                            }
                         }
-                        .frame(height: 15)
-                    }
+                        .transition(AnyTransition.slide.animation(Animation.easeInOut))
                 }
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color(UIColor(hex: "FDF0D5")))
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.89) {
-                    withAnimation {
-                        self.isActive = true
-                    }
-                }
+            .padding(.bottom, 100)
+            
+            if didUnlock {
+                ContentView()
+                    .transition(AnyTransition.slide.animation(Animation.easeInOut))
             }
+        }
+        .ignoresSafeArea()
+        .onAppear {
+            self.showUnlockedView = true
         }
     }
 }
